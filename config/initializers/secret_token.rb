f48e3656025913bc182e9456e55ec4cc2b480ccd
Will Fisher <will@teknofire.net>
2014-04-14 10:35:45 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GenericMap::Application.config.secret_key_base = 'abdc2fc0378d1af308b3c215aa8cdb1c5e41ceca6c670d0f8bb8bf13bdfdc1165e01cef96afce2c566ee70653529b9f69a0e0a78e7b908c77df19d62cd5f2657'
