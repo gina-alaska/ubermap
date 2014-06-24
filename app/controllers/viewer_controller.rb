2c2cf3e74d317e703ce50d9f6b1509fe91f58e67
Will Fisher <will@teknofire.net>
2014-06-23 16:17:15 -0800

    fixing viewer to work with small viewers

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,12 +1,12 @@
 class ViewerController < ApplicationController
+  after_action :allow_iframe!
+
   def show
     @map = Map.where('lower(slug) = ?', params[:slug]).first
 
     render layout: 'fullmap'
   end
   
-  after_action :allow_iframe!
-
   protected
 
   def allow_iframe!
