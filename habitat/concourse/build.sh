8568c95360259952167b2ac23bb537ccf886c3c9
Will Fisher <will@gina.alaska.edu>
2017-05-09 15:58:52 -0800

    adding outputs and release step

--- habitat/concourse/build.sh
+++ habitat/concourse/build.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-set -e
+set -xe
 
 pwd
-ls -la
 hab -V
 
 mkdir -p /hab/cache/keys
 echo -n "${HABITAT_ORIGIN_CONTENT}" > "/hab/cache/keys/${HABITAT_ORIGIN_FILENAME}"
 echo -n "${HABITAT_PUB_CONTENT}" > "/hab/cache/keys/${HABITAT_PUB_FILENAME}"
-
-ls -la /hab/cache/keys
-cat "/hab/cache/keys/${HABITAT_ORIGIN_FILENAME}"
 
 cd "${APPLICATION_PATH}"
 
