608d03235135c2c96cba1911d853f379fce244af
Will Fisher <will@gina.alaska.edu>
2017-05-09 15:25:09 -0800

    more debug output

--- habitat/concourse_build.sh
+++ habitat/concourse_build.sh
@@ -10,6 +10,7 @@ mkdir -p /hab/cache/keys
 echo "${HABITAT_ORIGIN_CONTENT}" > "/hab/cache/keys/${HABITAT_ORIGIN_FILENAME}"
 
 ls -la /hab/cache/keys
+cat "/hab/cache/keys/${HABITAT_ORIGIN_FILENAME}"
 
 cd "${APPLICATION_PATH}"
 
