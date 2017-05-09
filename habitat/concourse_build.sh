86148f94087f1582ee9c82b38922123e8ba084e2
Will Fisher <will@gina.alaska.edu>
2017-05-09 15:18:30 -0800

    clean up env var names

--- habitat/concourse_build.sh
+++ habitat/concourse_build.sh
@@ -8,7 +8,7 @@ hab -V
 
 mkdir -p /hab/cache/keys
 
-cat $HABITAT_ORIGIN_KEY > /hab/cache/keys/$HABITAT_ORIGIN_FILENAME
+cat $HABITAT_ORIGIN_CONTENT > /hab/cache/keys/$HABITAT_ORIGIN_FILENAME
 
 cd $APPLICATION_PATH
 
