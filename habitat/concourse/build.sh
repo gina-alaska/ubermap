fa291597c3892d3fc585adffb1e77e400d6e1276
Will Fisher <will@gina.alaska.edu>
2017-05-09 16:01:20 -0800

    do not use set -x when private info

--- habitat/concourse/build.sh
+++ habitat/concourse/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-set -xe
+set -e
 
+echo "*** DEBUG INFO ***"
 pwd
 hab -V
 
+echo "*** Create hab keys ***"
 mkdir -p /hab/cache/keys
 echo -n "${HABITAT_ORIGIN_CONTENT}" > "/hab/cache/keys/${HABITAT_ORIGIN_FILENAME}"
 echo -n "${HABITAT_PUB_CONTENT}" > "/hab/cache/keys/${HABITAT_PUB_FILENAME}"
 
+echo "*** Build package ***"
 cd "${APPLICATION_PATH}"
-
 hab pkg build . -k $HABITAT_ORIGIN_KEY
