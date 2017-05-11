935b217a70076723472b62ba7e5f2870e7f82422
Will Fisher <will@gina.alaska.edu>
2017-05-10 16:49:25 -0800

    update to pull down credentials

--- habitat/update_pipeline.sh
+++ habitat/update_pipeline.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env sh
-fly -t gina-main set-pipeline -p ubermap-habitat -c habitat/concourse.yml -l ~/.concourse/habitat.yml
+
+mkdir -p $HOME/.concourse
+knife vault download concourse habitat $HOME/.concourse/habitat.yml
+fly -t gina-main set-pipeline -p ubermap-habitat -c habitat/concourse.yml -l $HOME/.concourse/habitat.yml
+rm $HOME/.concourse/habitat.yml
